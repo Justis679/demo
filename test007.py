177f9df9dc9c628847da017118e3705085a40fea
Justis679 <jbisceglia276@gmail.com>
2025-01-09 13:59:29 +0900

    Create test007.py

---
+++ test007.py
@@ -0,0 +1,8 @@
+
+# 创建一个包含0到9的平方数的列表
+squares = [x**2 for x in range(10)]
+print(squares)  # 输出: [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+
+# 使用条件过滤
+even_squares = [x**2 for x in range(10) if x % 2 == 0]
+print(even_squares)  # 输出: [0, 4, 16, 36, 64]
