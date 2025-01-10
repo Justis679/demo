eb1519d231b3cebda167c0f047eddb4c90487851
Justis679 <jbisceglia276@gmail.com>
2025-01-10 23:30:47 +0900

    Update test008.py

--- test008.py
+++ test008.py
@@ -1,3 +1,7 @@
+"""
+用 get() 获取字典的值
+当访问字典的键时，如果键不存在，使用 get() 方法可以避免抛出 KeyError，并返回一个默认值（默认是 None）。
+"""
 person = {'name': 'Alice', 'age': 25}
 print(person.get('name'))  # 输出: Alice
 print(person.get('gender', 'Unknown'))  # 输出: Unknown
