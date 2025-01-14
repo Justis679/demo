221a9b8c59b34dd09686389068d838d07753c9fd
Justis679 <jbisceglia276@gmail.com>
2025-01-14 23:26:37 +0800

    Create test012.py

---
+++ test012.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+def generate_password(length=8):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+# 示例使用
+print("Generated password:", generate_password(12))
